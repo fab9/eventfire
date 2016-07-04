b7435878a1cacdf114263341a77b87b5aea6f3f1
fab9 <rorou192@gmail.com>
2016-07-04 10:00:30 -0400

    Add jQuery and file-validation.js

---
+++ src/scripts/form-validation.js
@@ -0,0 +1,10 @@
+/**
+ * Created by fab9 on 7/4/16.
+ */
+console.log("Form validation js starts");
+
+(function() {
+    var title = document.getElementById('title');
+
+    console.log("title classList: " + title.parentElement.classList);
+})();
